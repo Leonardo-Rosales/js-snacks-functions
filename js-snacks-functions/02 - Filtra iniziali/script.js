ee77421371a2c02f3432cc306a8fc4143857f4ae
Leonardo-Rosales <leonardorosaleshuerta@gmail.com>
2024-10-21 16:52:05 +0200

    add 2 esercizio

--- js-snacks-functions/02 - Filtra iniziali/script.js
+++ js-snacks-functions/02 - Filtra iniziali/script.js
@@ -5,9 +5,28 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 // Dichiara la funzione qui.
 
+function filterWordsByLetter(words, letter) {
+    let filteredWords = [];
+
+    for (let i = 0; i < words.length; i++) {
+        let word = words[i];
+
+        if (word.charAt(0) === letter) {
+            filteredWords.push(word);
+        }
+    }
+
+    return filteredWords;
+}
+
 
 // Invoca la funzione qui e stampa il risultato in console
 
+let startingLetter = "A"; 
+let result = filterWordsByLetter(names, startingLetter); 
 
+
+console.log(result); 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
+
